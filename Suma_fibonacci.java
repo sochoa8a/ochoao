0dd018701241a24ed11ef60c3aa916d46f73e820
Samuel Ochoa <samuel.ochoa@tarrsolutions.com>
2020-09-11 09:54:07 -0500

    Se agrega archivo java

---
+++ Suma_fibonacci.java
@@ -0,0 +1,24 @@
+package ejercicio_prueba;
+import java.util.Scanner; 
+
+public class Ejercicio_01 {
+
+	public static void main(String[] args) {
+		
+		Scanner leer = new Scanner(System.in);
+		int n, f = 0;
+		int t1 = 1;
+		int t2 = 0;
+		
+		System.out.print("Cuantos numeros desea generar?");
+		n = leer.nextInt();
+		
+		for(int i = 1; i<= n; i++) 
+		{
+			 t2 = f; 
+			 f = t1 + f;
+			 t1 = t2;
+			System.out.println(t1);
+		}
+	}
+}
